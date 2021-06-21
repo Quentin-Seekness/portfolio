5875872822f7c617c8ebcef677faba2427b98a42
Quentin Lemberton <awa.sboarding@gmail.com>
2021-06-21 17:03:29 +0200

    security of BO with loginForm and User

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -6,15 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends AbstractController
+class FrontController extends AbstractController
 {
     /**
-     * @Route("/", name="main")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('front/home.html.twig');
     }
 }
